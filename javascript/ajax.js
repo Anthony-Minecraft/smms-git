cc5c4eec3a9fe9fb8f71ab9906e8f648acadf649
Anthony-Minecraft <75374143+Anthony-Minecraft@users.noreply.github.com>
2022-12-17 14:03:39 -0500

    Added more styles and reload button

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,10 +1,22 @@
-function LoadDoc(URL) {
+function LoadDoc(URL, meterBar) {
+    let using_meter = false;
+    if (meterBar != null) {
+        using_meter = true;
+    }
+    move_progress(using_meter, meterBar, 1);
+    function move_progress(canMove, bar, amount) {
+        if (canMove) {
+            bar.value = amount;
+        }
+    }
     let Info = "";
     var xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         Info = this.responseText;
+        move_progress(using_meter, meterBar, 3);
     };
     xhttp.open("POST", URL, false);
+    move_progress(using_meter, meterBar, 2);
     xhttp.send();
     return Info;
 }
